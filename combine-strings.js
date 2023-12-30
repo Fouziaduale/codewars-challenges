c84dd31e5eaaa902ba9efc7d634153db79b6b219
Fouzia Duale <fouziaibrahim63@gmail.com>
2023-12-30 09:21:12 +0300

    added a new codewars challenge

---
+++ combine-strings.js
@@ -0,0 +1,15 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+
+// returns:
+
+// 'James Stevens'
+
+//My solution
+// Create the combineNames function here
+function combineNames(firstName, lastName){
+    return `${firstName} ${lastName}`
+  }
