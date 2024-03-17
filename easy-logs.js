3a3052a689c902ad2d34a36730e86392ec08b92d
Fouzia Duale <fouziaibrahim63@gmail.com>
2024-03-17 17:35:08 +0300

    added a new codewars challenge

---
+++ easy-logs.js
@@ -0,0 +1,6 @@
+// Given a logarithm base X and two values A and B, return a sum of logratihms with the base X: log⁡XA+log⁡XB 
+
+//My solution
+function  logs(x , a, b) {
+    return Math.log(a * b)/ Math.log(x)
+  }
