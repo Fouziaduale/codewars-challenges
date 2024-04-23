adc1720bc8e0e3de724068a717546e2a52623505
Fouzia Duale <fouziaibrahim63@gmail.com>
2024-04-23 09:21:00 +0300

    added a new codewars challenge

---
+++ a-letters-bestfriend.js
@@ -0,0 +1,33 @@
+// Given a string, return if a given letter always appears immediately before another given letter.
+
+// Worked Example
+
+// ("he headed to the store", "h", "e") ➞ True
+
+// # All occurences of "h": ["he", "headed", "the"]
+// # All occurences of "h" have an "e" after it.
+// # Return True
+
+// ('abcdee', 'e', 'e') ➞ False
+
+// # For first "e" we can get "ee"
+// # For second "e" we cannot have "ee"
+// # Return False
+
+// Examples
+
+// ("i found an ounce with my hound", "o", "u") ➞ True
+
+// ("we found your dynamite", "d", "y") ➞ False
+
+// Notes
+
+//     All sentences will be given in lowercase.
+
+//My solution
+function bestFriend(txt, a, b) {
+    for(let i = 0; i < txt.length; i++){
+      if(txt[i] === a && txt[i + 1] !== b) return false 
+    }
+    return true
+  }
