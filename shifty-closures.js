6b356ae1c60eb4ddede98b91e8f962ba3f67b0df
Fouzia Duale <fouziaibrahim63@gmail.com>
2024-06-13 07:21:00 +0300

    added a new codewars challenge

---
+++ shifty-closures.js
@@ -0,0 +1,7 @@
+// Functional closures can get overly attached. Set them straight!
+
+// Why doesn't greetAbe() actually greet Abe?
+
+//My solution
+const greet_abe = () => 'Hello, Abe!';
+const greet_ben = () => 'Hello, Ben!';
