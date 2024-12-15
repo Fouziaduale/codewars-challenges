052bbc779770e0c55c1650eb07fce05da05b53a7
Fouzia Duale <fouziaibrahim63@gmail.com>
2024-12-15 09:23:00 +0300

    added a new codewars challenge

---
+++ return-to-sanity.js
@@ -0,0 +1,7 @@
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+//My solution
+function mystery() {
+    let result = {'sanity': 'Hello'};
+    return result;
+  }
